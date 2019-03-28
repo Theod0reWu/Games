db8fa398ad55f69d3e944b87555780bd7a26893c
Theodore Wu <twu00@stuy.edu>
2019-03-28 19:55:54 -0400

    got started

--- Demo.java
+++ Demo.java
@@ -1,1 +1,34 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+import javax.swing.JPanel;
+
+public class Demo extends JFrame {
+  public class Board extends JPanel {
+
+      public Board() {}
+  }
+
+    public Demo() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Board());
+
+        setSize(500, 500);
+
+        setTitle("Demo");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            Demo ex = new Demo();
+            ex.setVisible(true);
+        });
+    }
+}
 

75c172756cad65448b31be1df0d9c8e810ca80c5
Theodore Wu <twu00@stuy.edu>
2019-03-28 20:16:38 -0400

    circles

--- Demo.java
+++ Demo.java
@@ -2,24 +2,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 import javax.swing.JPanel;
-
-public class Demo extends JFrame {
+/*
   public class Board extends JPanel {
 
       public Board() {}
   }
-
+*/
+public class Demo extends JFrame {
     public Demo() {
-
         initUI();
     }
 
     private void initUI() {
-
-        add(new Board());
-
+        add(new JPanel());
         setSize(500, 500);
-
         setTitle("Demo");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);

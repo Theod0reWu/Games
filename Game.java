1d860ec0a176d6bbc7341ed4b0e216952ac081d3
Theodore Wu <twu00@stuy.edu>
2019-03-31 11:32:37 -0400

    ball changes direction

--- Game.java
+++ Game.java
@@ -7,23 +7,49 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 public class Game extends JPanel{
 //http://www.edu4java.com/en/game/game1.html
+	int x = 0;
+	int y = 0;
+	int xa = 1;
+	int ya = 1;
+
+	private void moveBall() {
+		if (x + xa < 0)
+			xa = 1;
+		if (x + xa > getWidth() - 30)
+			xa = -1;
+		if (y + ya < 0)
+			ya = 1;
+		if (y + ya > getHeight() - 30)
+			ya = -1;
+		
+		x = x + xa;
+		y = y + ya;
+	}
+	public void move(){
+		x+=1;
+		y+=1;
+	}
 	@Override
 	public void paint(Graphics g) {
+		super.paint(g);
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(Color.RED);
-		g2d.fillOval(0, 0, 30, 30);
-		g2d.drawOval(0, 50, 30, 30);		
-		g2d.fillRect(50, 0, 30, 30);
-		g2d.drawRect(50, 50, 30, 30);
-
-		g2d.draw(new Ellipse2D.Double(0, 100, 30, 30));
+		g2d.setColor(Color.BLACK);
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.fillOval(x, y, 30, 30);
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException{
 		JFrame frame = new JFrame("Mini Tennis");
-		frame.add(new Game());
+		Game game = new Game();
+		frame.add(game);
 		frame.setSize(300, 300);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		while(true){
+			game.moveBall();
+			game.repaint();
+			Thread.sleep(5);
+		}
 	}
 }
